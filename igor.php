c06bd6c6daaae0185456a96ab9811b709ee558cc
igoryen <igor.yentaltsev@gmail.com>
2015-12-13 18:08:35 -0500

    main.css: clean up

--- igor.php
+++ igor.php
@@ -1,12 +1,10 @@
-<?php
-
-include_once 'header.php';
-
-
-
-foreach ($bag as $value) {
-  echo $value;
-}
-?>
+    <div class="container10">
+    <?php
+    include_once 'header.php';
+    foreach ($bag as $value) {
+      echo $value;
+    }
+    ?>
+    </div>
   </body>
 </html>
